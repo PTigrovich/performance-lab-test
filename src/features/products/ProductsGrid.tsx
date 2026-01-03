d540f74c5d314af1544ca6a36338cf7477f049c4
Paul_Mirzoyants <pavcnfh@yandex.ru>
2026-01-03 21:48:22 +0300

    bug fix sorting layer

--- src/features/products/ProductsGrid.tsx
+++ src/features/products/ProductsGrid.tsx
@@ -3,23 +3,38 @@ import { RootState } from '../../app/store'
 import ProductCard from './ProductCard'
 import { setPage } from './productsSlice'
 import styles from './ProductsGrid.module.scss'
+import { toggleSortOrder } from './productsSlice'
 
 const ProductsGrid = () => {
   const dispatch = useDispatch()
-  const { items, page, limit } = useSelector(
-    (state: RootState) => state.products
+  const { items, page, limit, sortOrder } = useSelector(
+  (state: RootState) => state.products
   )
   const category = useSelector(
     (state: RootState) => state.filters.category
   )
+  
 
   const filtered = items.filter((p) => p.category === category)
-  const start = (page - 1) * limit
-  const paginated = filtered.slice(start, start + limit)
-  const totalPages = Math.ceil(filtered.length / limit)
+
+const sorted = [...filtered].sort((a, b) => {
+  if (sortOrder === 'asc') {
+    return a.title.localeCompare(b.title)
+  }
+  return b.title.localeCompare(a.title)
+})
+
+const start = (page - 1) * limit
+
+
+const totalPages = Math.ceil(filtered.length / limit)
+const paginated = sorted.slice(start, start + limit)
 
   return (
     <>
+	 <button onClick={() => dispatch(toggleSortOrder())}>
+  		Сортировка: {sortOrder === 'asc' ? 'A → Z' : 'Z → A'}
+	 </button>
       <div className={styles.grid}>
         {paginated.map((product) => (
           <ProductCard key={product.id} product={product} />
--- src/features/products/productsSlice.ts
+++ src/features/products/productsSlice.ts
@@ -6,12 +6,14 @@ interface ProductsState {
     items: Product[];
     page: number;
     limit: number;
+    sortOrder: SortOrder;
 }
 
 const initialState: ProductsState = {
     items: mockProducts,
     page: 1,
     limit: 4,
+    sortOrder: 'asc',
 };
 
 const productsSlice = createSlice({
@@ -21,8 +23,14 @@ const productsSlice = createSlice({
         setPage(state, action) {
             state.page = action.payload;
         },
+        toggleSortOrder(state) {
+            state.sortOrder = state.sortOrder === 'asc' ? 'desc' : 'asc';
+            state.page = 1; // сбрасываем страницу — важный UX момент
+        },
     },
 });
 
-export const { setPage } = productsSlice.actions;
+
+export type SortOrder = 'asc' | 'desc';
+export const { setPage, toggleSortOrder } = productsSlice.actions;
 export default productsSlice.reducer;
