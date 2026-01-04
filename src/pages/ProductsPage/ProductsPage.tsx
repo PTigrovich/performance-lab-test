8b9abf755799b8331a978f4801c6ba79fac55e39
Paul_Mirzoyants <pavcnfh@yandex.ru>
2026-01-04 12:05:11 +0300

    bug fix pagination

--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useSearchParams } from 'react-router-dom'
 
+import { RootState } from '../../app/store'
 import FilterPanel from '../../features/filters/FilterPanel'
-import { setCategory } from '../../features/filters/filtersSlice'
-import { Category } from '../../entities/product/types'
 import ProductsGrid from '../../features/products/ProductsGrid'
+import { setCategory } from '../../features/filters/filtersSlice'
+import { setPage } from '../../features/products/productsSlice'
+import { Category } from '../../entities/product/types'
 
 import { toggleCart } from '../../features/cart/cartSlice'
 import CartSidebar from '../../features/cart/CartSidebar'
@@ -14,21 +16,33 @@ const ProductsPage = () => {
   const dispatch = useDispatch()
   const [searchParams] = useSearchParams()
 
+  const category = useSelector(
+    (state: RootState) => state.filters.category
+  )
+
+  // URL восстанавливаем из категории
   useEffect(() => {
-    const category = searchParams.get('category') as Category | null
-    if (category) {
-      dispatch(setCategory(category))
+    const categoryFromUrl = searchParams.get('category') as Category | null
+    if (categoryFromUrl) {
+      dispatch(setCategory(categoryFromUrl))
     }
   }, [dispatch, searchParams])
+
+  // смена категории- сброс страницы до 1
+  useEffect(() => {
+    dispatch(setPage(1))
+  }, [category, dispatch])
 
   return (
     <div>
       <FilterPanel />
       <ProductsGrid />
-		<button onClick={() => dispatch(toggleCart())}>
-  			Открыть корзину
-		</button>
-		<CartSidebar />
+
+      <button onClick={() => dispatch(toggleCart())}>
+        Открыть корзину
+      </button>
+
+      <CartSidebar />
     </div>
   )
 }
